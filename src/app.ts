703024a30f87edb0592ce5c8a89dba0e4aaf8fd8
Cpietro87 <cristianpietro2019@gmail.com>
2023-05-23 10:59:45 -0300

    se realizó rutas y  la conexion a mongo

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-import express from "express";
+import express, { Application} from "express";
+import morgan from "morgan";
+import authRoutes  from "./routes/auth";
+
+const app: Application = express();
+
+// settings
+app.set('port', 3000);
+
+// middlewares
+app.use(morgan('dev'));
+
+// Routes
+app.use(authRoutes)
+
+export default app;
